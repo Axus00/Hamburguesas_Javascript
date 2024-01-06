f605e17b6d34737f80cfd67ae3235700b27c4a8e
Fernando Gómez <141048801+Axus00@users.noreply.github.com>
2024-01-05 21:35:04 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 //Se llaman los id para crear la funcionalidad
+//Funcionalidad para cambiar tema de la web
+
+let body = document.querySelector("body")
 export class llamar{
     onChange(event){
         let select2 = document.getElementById("cambiarTema").value
         let body = document.querySelector("body")
         //condicional para cambiar de color el fondo
         if(select2 == 'Oscuro'){
-            body.style.backgroundColor = "#3333";
+            body.style.backgroundColor = "black";
             localStorage.setItem('color', 'Oscuro');
         } else if (select2 == 'Rosa'){
             body.style.backgroundColor = "lightpink";
             localStorage.setItem('color', 'lightpink');
         } else if(select2 == 'Claro'){
-            body.style.backgroundColor = "lightgray";
-            localStorage.setItem('color', 'lightgray');
+            body.style.backgroundColor = "lightyellow";
+            localStorage.setItem('color', 'lightyellow');
         }
     
         event.preventDefault();
     }
 
-    prueba(){
+
+
+/*     prueba(){
         console.log("prueba")
-    }
+    } */
 }
+
+//guardar el color
+try{
+    const color = localStorage.getItem('color');
+    console.log(color)
+
+    if(color === 'Oscuro'){
+        body.style.backgroundColor = "black";
+        console.log("a");
+    }
+    else if(color === 'Rosa'){
+        body.style.backgroundColor = "lightpink";
+        console.log("b");
+    } 
+    else if(color === 'Claro'){
+        body.style.backgroundColor = "lightyellow";
+        console.log("c");
+    };
+}catch{}
+
+
+//FUncionalidad para cambiar idioma de la web
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 //Se importan funciones de otro archivo script
 import { llamar } from "./app.js";
 const otro = new llamar()
+
 
 //Se crean los elementos de la página web
 let body = document.querySelector('body');
@@ -103,7 +105,7 @@ let objetoCartas = [
     {src: './images/hamburguesa-2.webp', precio: '$15.900', nombre: 'Kentucky Sandwich', descripcion: '1 Kentucky hamburguesa / Sandwich (1 Filete de prechuga de pollo apanado, pepinillos...)'},
     {src: './images/hamburguesa-3.webp', precio: '$15.900', nombre: 'Combo BBQ Crunch', descripcion: '1 sandwich BBQ Crunch (1 Filete de pollo apanado) + 1 papa pequeña + 1 gaseosa PET...'},
     {src: './images/hamburguesa-4.webp', precio: '$16.900', nombre: 'Kentucky Coronel Sandwich', descripcion: '1 Kentucky Coronel hamburguesa / Sandwich (1 Filete de pechuga de pollo apanado, ensalada...)' },
-    {src: './images/hamburguesa-5.webp', nombre: 'Combo Kentucky Sandwich', descripcion: '1 Kentucky hamburguesa / Sandwich (1 Filete de prechuga de pollo apanado, pepinillos...)'},
+    {src: './images/hamburguesa-5.webp', precio: '$21.900', nombre: 'Combo Kentucky Sandwich', descripcion: '1 Kentucky hamburguesa / Sandwich (1 Filete de prechuga de pollo apanado, pepinillos...)'},
     {src: './images/hamburguesa-6.webp', precio: '$22.900', nombre: 'Combo Kentucky Coronel Sandwich', descripcion: '1 Kentucky Coronel hamburguesa / Sandwich (1 Filete de pechuga de pollo apanado, ensalada...)'},
     {src: './images/hamburguesa-7.webp', precio: '$23.900', nombre: 'Sandwich Crispy BBQ', descripcion: '1 Sandwich Crispy BBQ (1 Filete de pechuga extra grande, triple empanizado, cebolla crisp...)'},
     {src: './images/hamburguesa-8.webp', precio: '$29.900', nombre: 'Combo Sandwich Crispy BBQ', descripcion: '1 Sandwich Crispy BBQ (1 Filete de pechuga extra grande, triple empanizado, cebolla crisp...)'},
@@ -142,6 +144,7 @@ card2.appendChild(precio);
 
 //h1
 let nombre = document.createElement('h1');
+nombre.id = 'nombre';
 nombre.textContent = objetoCartas[i % objetoCartas.length].nombre;
 card2.appendChild(nombre)
 
@@ -172,5 +175,14 @@ console.log(body);
 
 //Se llama nuevamente los id para darle la funcionalidad a los selectores de la página
 
-let este = document.getElementById("cambiarTema")
-este.addEventListener("change", otro.onChange)
+let este = document.getElementById("cambiarTema");
+este.addEventListener("change", otro.onChange);
+
+
+
+
+
+
+
+
+
